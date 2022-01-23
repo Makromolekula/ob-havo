39f9cfd22bf6d820a05bac243faeb2b11cdef7fa
Makromolekula <qodirberdinazarov@mail.ru>
2022-01-23 20:35:40 +0500

    second

---
+++ second/app.js
@@ -0,0 +1,27 @@
+const github = new Github;
+
+const ui = new UI;
+
+const searchUser = document.getElementById('searchUser');
+
+searchUser.addEventListener('keyup', (e) => {
+  userText = e.target.value;
+
+  if(userText !== ''){
+    //make http call
+    github.getUser(userText)
+     .then(data => {
+      if(data.profile.message === 'Not Found'){
+        //show alert
+        ui.showAlert('User not found', 'alert alert-danger');
+      }else{
+        //show profile
+        ui.showProfile(data.profile);
+        ui.clearAlert();
+      }
+     })
+  }else{
+    //clear profile
+    ui.clearProfile();
+  }
+});
